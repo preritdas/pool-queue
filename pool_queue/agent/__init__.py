c9ea2f0961f8c484b80a37fd8daafa7c9e5691ea
Prerit Das <preritdas@gmail.com>
2023-07-20 20:36:47 +0000

    Implement custom zeroshot agent.

---
+++ pool_queue/agent/__init__.py
@@ -0,0 +1,15 @@
+"""The agent to serve as the interface between users and the pool-queue system."""
+from langchain.agents import Tool, ZeroShotAgent, AgentExecutor
+
+
+class InternalThoughtZeroShotAgent(ZeroShotAgent):
+    """
+    A normal ZeroShotAgent but doesn't inject "Thought:" before the LLM. After testing
+    and heavy prompt engineering, I've found a better sucess rate with having the LLM
+    create its own "Thought" label. This is because it knows that each Thought must
+    also have either an Action/Action Input or a Final Answer.
+    """
+    @property
+    def llm_prefix(self) -> str:
+        """Prefix to append the llm call with."""
+        return ""
