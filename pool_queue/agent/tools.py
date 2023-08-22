86aa80e7978c1faa0a7ef1eb87c980358251e749
Prerit Das <preritdas@gmail.com>
2023-08-22 01:27:54 +0000

    All tools must have input.

--- pool_queue/agent/tools.py
+++ pool_queue/agent/tools.py
@@ -16,11 +16,14 @@ def create_registration_tool(player_phone: str) -> BaseTool:
         name = "Register Player"
         description = (
             "Register a new player with the system. Input must be simply the full name of "
-            "the player. If the full name is not provided, ask the user for the full name."
+            "the player. If the full name is not provided, ask the user for the full name. "
+            "Provide the full name with proper capitalization, even if it wasn't provided "
+            "that way. Ex. if user provides 'john doe', you should register 'John Doe'."
         )
 
         def _run(self, name: str):
             Player.register(name=name, phone_number=player_phone)
+            return "Player has been registered."
 
     return RegisterPlayerTool()
 
@@ -48,9 +51,9 @@ def create_tools(player: Player) -> list[BaseTool]:
     class JoinQueueTool(BaseTool):
         """Join the queue."""
         name = "Join Queue"
-        description = "Join the queue to play a game."
+        description = "Join the queue to play a game. Takes no input, so input n/a."
 
-        def _run(self):
+        def _run(self, query: str):
             if not player_queue.add(player):
                 return (
                     "Player is already in the queue, position "
@@ -62,9 +65,9 @@ def create_tools(player: Player) -> list[BaseTool]:
     class LeaveQueueTool(BaseTool):
         """Leave the queue."""
         name = "Leave Queue"
-        description = "Leave the queue."
+        description = "Leave the queue. Takes no input, so input n/a."
 
-        def _run(self):
+        def _run(self, query: str):
             if not player_queue.remove(player):
                 return "Player was not in the queue."
 
@@ -73,9 +76,9 @@ def create_tools(player: Player) -> list[BaseTool]:
     class CheckPositionTool(BaseTool):
         """Check position in queue."""
         name = "Check Position"
-        description = "Check position in queue."
+        description = "Check position in queue. Takes no input, so input n/a."
 
-        def _run(self):
+        def _run(self, query: str):
             position = player_queue.get_position(player)
             if position == -1:
                 return "Player is not in the queue."
@@ -85,9 +88,12 @@ def create_tools(player: Player) -> list[BaseTool]:
     class SeeFullQueueTool(BaseTool):
         """See the full queue."""
         name = "See Full Queue"
-        description = "See the names of all players in order in the queue."
+        description = (
+            "See the names of all players in order in the queue. "
+            "Takes no input, so input n/a."
+        )
 
-        def _run(self):
+        def _run(self, query: str):
             queue: list[Player] = player_queue.get_queue()
             queue_str = "\n".join([f"{i}. {p.name}" for i, p in enumerate(queue, 1)])
             return f"Queue:\n{queue_str}"
@@ -125,10 +131,11 @@ def create_tools(player: Player) -> list[BaseTool]:
         description = (
             "The loser of the active match will use this tool to specify that they lost "
             "the match. The game will then be ended and a new game will be started with "
-            "the next player in the queue to challenge the winner of the last match."
+            "the next player in the queue to challenge the winner of the last match. "
+            "Takes no input, as the loser is the one to use this tool, so input n/a."
         )
 
-        def _run(self):
+        def _run(self, query: str):
             try:
                 game = Game.from_only_active()
             except GameNotFoundError:
@@ -166,10 +173,11 @@ def create_tools(player: Player) -> list[BaseTool]:
         description = (
             "The winner of the last match will use this tool to confirm the next player "
             "in the queue as the challenger. This tool will only work if the next player "
-            "in the queue has used the 'Start Game' tool and is waiting to be confirmed."
+            "in the queue has used the 'Start Game' tool and is waiting to be confirmed. "
+            "Takes no input, as the winner is the one to use this tool, so input n/a."
         )
 
-        def _run(self):
+        def _run(self, query: str):
             try:
                 game = Game.from_only_pending()
             except GameNotFoundError:
